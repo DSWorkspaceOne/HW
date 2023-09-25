d44ac7bd73cd0f2a4a2aaa0a12108bfdfca43eca
Daria <psixeaaa@yandex.ru>
2023-09-25 17:23:18 +0300

    new files

---
+++ program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №1 
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int str = number * number;
+
+Console.Write("Квадрат числа: ");
+Console.Write(number);
+Console.Write(" равен = ");
+Console.Write(str);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ program1.cs
@@ -0,0 +1,3 @@
+Console.Write("Введите  второе число ");
+int byte number2 = int.Parse(Console.ReadLine());
+int Max = (byte number1, byte number2);
